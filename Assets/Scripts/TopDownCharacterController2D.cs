e9bb67daf195a2e233ace86d5532aff69b2284ba
liamthomas1 <55701056+liamthomas1@users.noreply.github.com>
2019-12-08 06:03:24 +0000

    part11

--- Assets/Scripts/TopDownCharacterController2D.cs
+++ Assets/Scripts/TopDownCharacterController2D.cs
@@ -4,11 +4,11 @@ using UnityEngine;
 
 public class TopDownCharacterController2D : MonoBehaviour
 { public float speed = 5.0f;
-    Rigidbody2D rigidbody2D;
+    Rigidbody2D Rigidbody2D;
     // Start is called before the first frame update
     void Start()
     {
-        rigidbody2D = GetComponent<Rigidbody2D>();
+        Rigidbody2D = GetComponent<Rigidbody2D>();
         
     }
 
@@ -17,8 +17,8 @@ public class TopDownCharacterController2D : MonoBehaviour
     {
         float x = Input.GetAxis("Horizontal");
         float y = Input.GetAxis("Vertical");
-        rigidbody2D.velocity = new Vector2(x, y) * speed;
-        rigidbody2D.angularVelocity = 0.0F;
+        Rigidbody2D.velocity = new Vector2(x, y) * speed;
+        Rigidbody2D.angularVelocity = 0.0F;
 
         
     }
